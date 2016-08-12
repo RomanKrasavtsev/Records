5a046feae146ed28e9a9fe05f4d00ba26ad89fab
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-08-12 16:41:22 +0300

    update records

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,4 @@
 class Record < ApplicationRecord
+  belongs_to :user
+  has_one :item
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802120236) do
+ActiveRecord::Schema.define(version: 20160812122502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "record_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_id"], name: "index_items_on_record_id", using: :btree
+  end
 
   create_table "records", force: :cascade do |t|
     t.string   "title"
     t.string   "kind"
     t.date     "date"
     t.float    "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
